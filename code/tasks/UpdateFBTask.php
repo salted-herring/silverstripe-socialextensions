6905ed3471f379c5814501fee870c57fa1d697bf
Simon Winter <simon@saltedherring.com>
2014-08-07 13:08:58 +1200

    Added build task

---
+++ code/tasks/UpdateFBTask.php
@@ -0,0 +1,25 @@
+<?php
+
+class UpdateFBTask extends BuildTask {
+ 
+    protected $title = 'Update Facebook OG Data';
+ 
+    protected $description = 'Update Facebook Open Graph data on objects that implement FBInterface';
+ 
+    protected $enabled = true;
+ 
+    public function run($request) {
+        $data = DataObject::get();
+        
+        echo 'Updating Open Graph Data ...<br>';
+        
+        foreach($data as $record) {
+        	if(cin_array("FBInterface", class_implements($record->ClassName))) {
+	        	$record->forceChange();
+				$record->write();
+        	}
+        }
+        
+       echo 'Updated ' . $team->count() . ' records.';
+    }
+}
