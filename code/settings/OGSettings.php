20aca135effe00e88974bcefd6947f18aa6d7cab
Simon Winter <simon@saltedherring.com>
2015-05-08 13:12:07 +1200

    Updates to social tabs in admin

--- code/settings/OGSettings.php
+++ code/settings/OGSettings.php
@@ -19,19 +19,20 @@
 		 * @param FieldList $fields
 		 */
 		public function updateCMSFields(FieldList $fields) {
-			//
-			// Generic OG Tags.
-			//
-			$fields->addFieldsToTab('Root.Social.OpenGraph', array(
-				$title = new TextField('OGTitle', 'Title'),
-				$description = new TextareaField('OGDescription', 'Description'),
-				$img = new UploadField('OGImage', 'Image')
+
+			$fields->addFieldsToTab('Root.Social', array(
+				new Tab(
+					$title = 'OpenGraph',
+					new TextField('OGTitle', 'Title'),
+					new TextareaField('OGDescription', 'Description'),
+					new UploadField('OGImage', 'Image')
+				),
+				new Tab(
+					$title = 'Twitter',
+					$name = new TextField('TwitterName', 'Twitter Name')
+				)
 			));
 			
-			//
-			// Twitter
-			//
-			$fields->addFieldToTab('Root.Social.Twitter', $name = new TextField('TwitterName', 'Twitter Name'));
 			$name->setDescription('Your account name that content will be shared from (don\'t include the @).');
 		}
 	
